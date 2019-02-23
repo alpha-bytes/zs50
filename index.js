79dc34296d73b3e20d49689bd5cf4e07663ce8a3
Kevin Rice <kevinrice@Kevins-MacBook-Pro.local>
2019-02-22 22:00:53 -0600

    fs module

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const bash = require('child_process').exec; 
+const fs = require('fs');
 
 module.exports = () => {
     const cmd = 'ls'; 
     bash(cmd, (error, stdout, stderr) =>{
         if(stderr)
-            console.error(stderr);
-        if(stdout)
-            console.log(stdout);
+            return console.error(stderr);
+        
+        // read contents of file
+        fs.readFile('package.json', 'utf-8', (err, data) =>{
+            console.log(data); 
+        }); 
+        
     })
 }; 
