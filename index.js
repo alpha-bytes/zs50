342cd776601940d2aea86b8af12c0bc5bbfc7ecd
Kevin Rice <kevinrice@Kevins-MacBook-Pro.local>
2019-02-22 21:26:44 -0600

    poc

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = () => console.log(process.argv.splice(2)); 
+const bash = require('child_process').exec; 
+
+module.exports = () => {
+    const cmd = 'ls'; 
+    bash(cmd, (error, stdout, stderr) =>{
+        if(stderr)
+            console.error(stderr);
+        if(stdout)
+            console.log(stdout);
+    })
+}; 
