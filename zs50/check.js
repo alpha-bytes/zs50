8e393469d119c9f27675909742c4db9330c58d24
Kevin Rice <kevinrice@Kevins-MacBook-Pro.local>
2019-03-07 13:42:50 -0600

    removed modules

--- zs50/check.js
+++ zs50/check.js
@@ -1,7 +1,7 @@
 const bash = require('child_process').exec; 
 const fs = require('fs');
 
-module.exports = function (pset, options) {
+module.exports = function(pset, options) {
     console.log(pset); 
     console.log(options.username); 
 }
